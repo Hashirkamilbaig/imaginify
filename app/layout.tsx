e67fb20b5d566616b4a618c4cf3501f77cba5bb3
hashirkamilbaig <hashirkamilbaig@gmail.com>
2024-11-14 20:30:02 -0800

    removed some local fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,7 @@ import "./globals.css";
 import { IBM_Plex_Sans } from "next/font/google" 
 import { cn } from "@/lib/utils";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
