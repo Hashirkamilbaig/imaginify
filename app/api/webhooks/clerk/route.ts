bd9e6695ce977752444985116e50cbb9ff59a11e
hashirkamilbaig <hashirkamilbaig@gmail.com>
2024-11-21 17:11:54 -0800

    adding webhooks again, fixing mongodb database, added prisma and creating user through prisma

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,9 +1,11 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser } from '@/lib/actions/user.actions'
+import { User } from '@prisma/client'
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.Webhook_SECRET
+  const SIGNING_SECRET = process.env.SIGNING_SECRET
 
   if (!SIGNING_SECRET) {
     throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
@@ -13,12 +15,10 @@ export async function POST(req: Request) {
   const wh = new Webhook(SIGNING_SECRET)
 
   // Get headers
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
-  console.log(headerPayload);
-
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -49,10 +49,29 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
   const eventType = evt.type
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+  console.log(`Received webhook with ID ${evt.data.id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
+  
+  if(eventType === 'user.created'){
+    const {id, email_addresses, first_name, last_name, image_url} = evt.data
+
+    if(!id || !email_addresses){
+      return new Response('Error occured -- missing data', {
+        status: 400
+      })
+    }
+
+    const user = {
+      clerkUserId: id,
+      email: email_addresses[0].email_address,
+      ...(first_name ?  {firstName: first_name} : {}),
+      ...(last_name ? {lastName: last_name} : {}),
+      ...(image_url ? { imageUrl: image_url} : {})
+    }
+
+    await createUser(user as User)
+  }
 
   return new Response('Webhook received', { status: 200 })
 }
