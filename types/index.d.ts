4ff99ea609e687ff0aa0cb022bc1c5b4bc498597
hashirkamilbaig <hashirkamilbaig@gmail.com>
2024-11-24 02:18:54 -0800

    trying something new with database AGIAN4

--- types/index.d.ts
+++ types/index.d.ts
@@ -114,8 +114,11 @@ declare type RemoveUrlQueryParams = {
 };
 
 declare type SearchParamProps = {
-  params: { id: string; type: TransformationTypeKey };
-  searchParams: { [key: string]: string | string[] | undefined };
+  params: {
+    id: string; 
+    type: TransformationTypeKey; 
+  };
+  searchParams?: Record<string, string | string[] | undefined>;
 };
 
 declare type TransformationFormProps = {
